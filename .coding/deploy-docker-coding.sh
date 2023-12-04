cdeb57f0ebf988416522fb0eed255caa555db4f3
OrdinaryRoad <or-mjz@qq.com>
2023-12-04 21:23:16 +0800

    build: 版本号更新

--- .coding/deploy-docker-coding.sh
+++ .coding/deploy-docker-coding.sh
@@ -15,7 +15,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-export APP_VERSION=1.0.6
+export APP_VERSION=1.0.7
 
 docker login ordinaryroad-docker.pkg.coding.net
 
--- barrage-fly/build.gradle.kts
+++ barrage-fly/build.gradle.kts
@@ -25,7 +25,7 @@ plugins {
 }
 
 group = "tech.ordinaryroad"
-version = "1.0.6"
+version = "1.0.7"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_17
