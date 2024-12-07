ce1aa3eb5ef0fd77866f0fcc1f05689b2206204c
OrdinaryRoad <1962247851@qq.com>
2024-12-07 11:11:44 +0800

    cicd

--- .coding/build-images.sh
+++ .coding/build-images.sh
@@ -15,6 +15,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+docker login ordinaryroad-docker.pkg.coding.net
+
 export APP_VERSION=1.5.0
 
 # 1 后端
@@ -22,10 +24,17 @@ cd ../barrage-fly
 rm -rf ./src/main/resources/public/*
 ./gradlew clean bootJar
 cd ../.docker/ordinaryroad-barrage-fly
-docker build . -f Dockerfile -t ordinaryroad-barrage-fly:${APP_VERSION}
-docker tag ordinaryroad-barrage-fly:${APP_VERSION} ordinaryroad-barrage-fly
-docker build . -f Dockerfile-arm64 -t ordinaryroad-barrage-fly-arm64:${APP_VERSION}
-docker tag ordinaryroad-barrage-fly-arm64:${APP_VERSION} ordinaryroad-barrage-fly-arm64
+
+# 构建并发布多平台版本
+docker buildx build --platform linux/arm64,linux/amd64 --push . -f Dockerfile -t ordinaryroad-docker.pkg.coding.net/ordinaryroad-barrage-fly/docker-pub/ordinaryroad-barrage-fly:${APP_VERSION}
+docker buildx build --platform linux/arm64,linux/amd64 --push . -f Dockerfile -t ordinaryroad-docker.pkg.coding.net/ordinaryroad-barrage-fly/docker-pub/ordinaryroad-barrage-fly
+
+# 更新本地镜像
+docker pull ordinaryroad-docker.pkg.coding.net/ordinaryroad-barrage-fly/docker-pub/ordinaryroad-barrage-fly:${APP_VERSION}
+docker tag ordinaryroad-docker.pkg.coding.net/ordinaryroad-barrage-fly/docker-pub/ordinaryroad-barrage-fly:${APP_VERSION} ordinaryroad-barrage-fly:${APP_VERSION}
+docker pull ordinaryroad-docker.pkg.coding.net/ordinaryroad-barrage-fly/docker-pub/ordinaryroad-barrage-fly
+docker tag ordinaryroad-docker.pkg.coding.net/ordinaryroad-barrage-fly/docker-pub/ordinaryroad-barrage-fly ordinaryroad-barrage-fly
+
 
 # 2 前端
 cd ../../barrage-fly-ui
@@ -35,15 +44,13 @@ npm install
 npm run build
 cd ..
 
-docker login ordinaryroad-docker.pkg.coding.net
-
 # 构建并发布多平台版本
 docker buildx build --platform linux/arm64,linux/amd64 --push . -f Dockerfile -t ordinaryroad-docker.pkg.coding.net/ordinaryroad-barrage-fly/docker-pub/ordinaryroad-barrage-fly-ui:${APP_VERSION}
 docker buildx build --platform linux/arm64,linux/amd64 --push . -f Dockerfile -t ordinaryroad-docker.pkg.coding.net/ordinaryroad-barrage-fly/docker-pub/ordinaryroad-barrage-fly-ui
 
+# 更新本地镜像
 docker pull ordinaryroad-docker.pkg.coding.net/ordinaryroad-barrage-fly/docker-pub/ordinaryroad-barrage-fly-ui:${APP_VERSION}
 docker tag ordinaryroad-docker.pkg.coding.net/ordinaryroad-barrage-fly/docker-pub/ordinaryroad-barrage-fly-ui:${APP_VERSION} ordinaryroad-barrage-fly-ui:${APP_VERSION}
-
 docker pull ordinaryroad-docker.pkg.coding.net/ordinaryroad-barrage-fly/docker-pub/ordinaryroad-barrage-fly-ui
 docker tag ordinaryroad-docker.pkg.coding.net/ordinaryroad-barrage-fly/docker-pub/ordinaryroad-barrage-fly-ui ordinaryroad-barrage-fly-ui
 
